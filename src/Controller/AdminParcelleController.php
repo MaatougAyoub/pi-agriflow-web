feccb03999e45faf8d7e90086312fc2491b7462b
BadisBeji <badisbeji0@gmail.com>
2026-04-05 21:53:04 +0100

    integration parcelles & cultures 2

--- src/Controller/AdminParcelleController.php
+++ src/Controller/AdminParcelleController.php
@@ -11,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/parcelles', name: 'app_admin_parcelle_')]
+#[IsGranted('ROLE_ADMIN')]
 final class AdminParcelleController extends AbstractController
 {
     #[Route('', name: 'index', methods: ['GET'])]
